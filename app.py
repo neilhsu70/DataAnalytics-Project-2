bd6f73e51c09b83d317868ca81ee7da545188230
Ningning Du <ningningdu@ningnings-mbp.lan>
2020-06-11 21:08:53 -0500

    push files for Heroku

---
+++ app.py
@@ -0,0 +1,120 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+
+import plotly
+
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import plotly.graph_objects as go
+import plotly.express as px
+#from scipy.interpolate import interp1d
+from datetime import datetime
+
+#Data
+death_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
+confirmed_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+recovered_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
+country_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv')
+
+death_df.drop('Province/State', axis=1, inplace=True)
+confirmed_df.drop('Province/State', axis=1, inplace=True)
+recovered_df.drop('Province/State', axis=1, inplace=True)
+country_df.drop(['People_Tested', 'People_Hospitalized'], axis=1, inplace=True)
+
+death_df.rename(columns={'Country/Region': 'Country'}, inplace=True)
+confirmed_df.rename(columns={'Country/Region': 'Country'}, inplace=True)
+recovered_df.rename(columns={'Country/Region': 'Country'}, inplace=True)
+country_df.rename(columns={'Country_Region': 'Country', 'Long_': 'Long'}, inplace=True)
+country_df.sort_values('Confirmed', ascending=False, inplace=True)
+
+bubble_fig = px.scatter(country_df.head(10), x = 'Country', y ='Confirmed', size = 'Confirmed', color = 'Country', hover_name = 'Country',size_max = 60)
+           
+#get navbar from navbar.py
+
+#get heading and what is covid from heading.py
+
+#get plots from plots.py file
+from plots import global_animation, us_bar
+
+#data
+
+#headings
+
+tally_heading = html.H2(children='World Cases Tally', className='mt-5 py-4 pb-3 text-center')
+global_map_heading = html.H2(children='World outbreaks of COVID-19 across time', className='mt-5 py-4 pb-3 text-center')
+us_heading =  html.H2(children='US Cases: Confirmed and Deaths', className='mt-5 py-4 pb-3 text-center')
+# Initialize the app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.JOURNAL])
+
+
+#Define the app
+app.layout = html.Div([
+
+    dbc.Container([
+        html.H1(children='COVID-19 Pandemic Analysis Dashboard', className='mt-5 py-4 pb-3 text-center')
+    ]),
+
+    dbc.Row(
+            [
+                dbc.Col(children = [html.H4('Confirmed', style = {'padding-top': '5px'}),
+                        html.Div([dbc.Button(country_df['Confirmed'].sum(), color="danger", size = "lg")])],
+                        width=3, className='text-center'),
+                
+                dbc.Col(children = [html.H4('Recovered', style = {'padding-top': '5px'}),
+                        html.Div([dbc.Button(country_df['Recovered'].sum(), color="success", size = "lg")])],
+                        width=3, className='text-center'),
+                
+                dbc.Col(children = [html.H4('Death', style = {'padding-top': '5px'}),
+                        html.Div([dbc.Button(country_df['Deaths'].sum(), color="primary", size = "lg")])],
+                        width=3, className='text-center'),
+                
+                dbc.Col(children = [html.H4('Active', style = {'padding-top': '5px'}),
+                        html.Div([dbc.Button(country_df['Active'].sum(), color="info", size = "lg")])],
+                        width=3, className='text-center'),
+            ], className='justify-content-center'),
+    
+   dbc.Row(
+       [
+          dbc.Col(
+              dbc.Container([
+                html.Div([
+                html.H2('What is COVID-19?', className='mt-5 py-4 pb-3 text-center'),
+                html.P("COVID-19 is a disease caused by a new strain of coronavirus. 'CO' stands for corona, 'VI' for virus, and 'D' for disease."),
+                html.P("Symptoms can include fever, cough and shortness of breath. In more severe cases, infection can cause pneumonia or breathing difficulties. More rarely, the disease can be fatal."),
+                html.P("The virus is transmitted through direct contact with respiratory droplets of an infected person (generated through coughing and sneezing)."),
+                html.P("Dashboard contributed by: Bianca Hernandez, Ningning Du, Neil Hsu, Youngjung Choi", style = {'font-weight': 'bold'}),
+                html.Div(dcc.Graph(figure=bubble_fig))    
+                ])])),
+
+          dbc.Col(
+              dbc.Container([global_map_heading,
+                html.Div(id='global-total'), 
+                dcc.Graph(
+                    id='global-viz',
+                    figure=global_animation()
+                    )           
+                ]
+            )
+          ) 
+       ]
+   ), 
+                          
+
+    dbc.Container([us_heading, 
+        html.Div(id='us-total'),
+            dcc.Graph(
+                id='us-viz',
+                figure=us_bar()  
+            )
+        ]
+    ),  
+]
+)   
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
